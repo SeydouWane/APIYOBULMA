ba65a57e95e433432b5c4596e9bcdfc7fece2a77
Papa Seydou WANE <129470066+SeydouWane@users.noreply.github.com>
2025-12-19 20:57:42 +0000

    Refactor create_order to use model_dump and UUID
    Updated create_order to use model_dump() for Pydantic v2 and added a unique ID.

--- routes/orders.py
+++ routes/orders.py
@@ -3,14 +3,17 @@ from sqlalchemy.ext.asyncio import AsyncSession
 from database.db import get_db
 from models.schemas import OrderCreate
 from database.models import OrderDB
-
+import uuid
 
 router = APIRouter()
 
 @router.post("/orders/")
 async def create_order(order_data: OrderCreate, db: AsyncSession = Depends(get_db)):
+    # Utilisation de model_dump() pour Pydantic v2+ 
+    # Ajout d'un ID unique et de données par défaut pour le test
     new_order = OrderDB(
-        **order_data.dict(),
+        **order_data.model_dump(),
+        id=str(uuid.uuid4()),
         seller_id="TEMP_SELLER_ID", 
         otp="1234" 
     )
